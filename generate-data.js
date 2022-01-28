3ac61f42dfd1a46025574141e9524f927fe0dc64
Richard Zilahi <richard.zilahi@varjo.com>
2022-01-28 23:18:29 +0200

    every schema has now mock data

--- generate-data.js
+++ generate-data.js
@@ -3,9 +3,11 @@ import random from 'random';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
+const getRandomSalary = () => random.int(2200, 7500);
+
 const tables = {
     employees: [
-        // { name: 'emp_no', randomFunction: uuidv4 },
+        { name: 'emp_no', randomFunction: undefined },
         { name: 'birth_data', randomFunction: faker.date.past },
         { name: 'fist_name', randomFunction: faker.name.firstName },
         { name: 'last_name', randomFunction: faker.name.lastName },
@@ -13,41 +15,68 @@ const tables = {
         { name: 'hire_date', randomFunction: faker.date.past },
     ],
     departments: [
-        { name: 'dept_no', randomFunction: uuidv4 },
+        { name: 'dept_no', randomFunction: undefined },
         { name: 'dept_name', randomFunction: faker.commerce.department },
     ],
-    
-}
-
-const generateRandomUserIds = (length) => Array.from({length}).map(() => uuidv4());
-
-const randomColumns = {
-    randomData: [
-        { name: 'department', randomFunction: faker.commerce.department },
-        { name: 'firstName', randomFunction: faker.name.firstName },
-        { name: 'lastName', randomFunction: faker.name.lastName },
-        { name: 'jobTitle', randomFunction: faker.name.jobTitle },
-        { name: 'birth_date', randomFunction: faker.date.past },
-        { name: 'gender', randomFunction: faker.name.gender },
-        { name: 'salaries', randomFunction: faker.finance.amount },
+    dept_emp: [
+        { name: 'emp_no', randomFunction: undefined },
+        { name: 'dept_no', randomFunction: undefined },
+        { name: 'from_data', randomFunction: faker.date.past },
+        { name: 'to_date', randomFunction: faker.date.future },
+    ],
+    dept_manager: [
+        { name: 'emp_no', randomFunction: undefined },
+        { name: 'dept_no', randomFunction: undefined },
+        { name: 'from_data', randomFunction: faker.date.past },
+        { name: 'to_date', randomFunction: faker.date.future },
+    ],
+    titles: [
+        { name: 'emp_no', randomFunction: undefined },
+        { name: 'title', randomFunction: faker.name.jobTitle },
+        { name: 'from_data', randomFunction: faker.date.past },
+        { name: 'to_date', randomFunction: faker.date.future },
+    ],
+    salaries: [
+        { name: 'emp_no', randomFunction: undefined },
+        { name: 'salary', randomFunction: getRandomSalary},
+        { name: 'from_data', randomFunction: faker.date.past },
+        { name: 'to_date', randomFunction: faker.date.future },
     ]
 }
+
+/* export const objectSchemas = {
+    'employees': Object.assign({}, ...tables.employees.map(field => ({
+        [field.name]: '',
+    }))),
+    'departments': Object.assign({}, ...tables.departments.map(field))
+} */
+
+// console.log(objectSchemas)
+
+const generateRandomUuids = (length) => Array.from({length}).map(() => uuidv4());
 
 function generateRandomData() {
     return new Promise((resolve) => {
         const randomNumber = 2 // random.int(10, 99);
-        const randomUserIds = generateRandomUserIds(randomNumber);
+        const randomUserIds = generateRandomUuids(randomNumber);
+        const randomDepartmentIds = generateRandomUuids(randomNumber);
+
+        const randomIds = {
+            'emp_no': [...randomUserIds],
+            'dept_no': [...randomDepartmentIds]
+        }
         Object.keys(tables).map(key => {
             const thisKey = tables[key]
             let randomData = []
-            console.log('thisKey', thisKey);
+            // console.log('thisKey', thisKey);
             if (Array.isArray(thisKey)) {
                 Array.from({ length: randomNumber }).fill().map((_, index) => {
                     let thisRandomObject = {}
                     Object.assign({}, thisKey.forEach((field) => {
-                        thisRandomObject[field.name] = field.randomFunction()
+                        console.log('typeof', typeof field.randomFunction)
+                        thisRandomObject[field.name] = !!field.randomFunction ? field.randomFunction() : randomIds[field.name][index]
                     }))
-                    randomData.push(Object.assign(thisRandomObject, {emp_no: randomUserIds[index] }));
+                    randomData.push(Object.assign(thisRandomObject));
                 });
             }
             writeJsonFiles(randomData, key);
--- schemas.js
+++
@@ -1,5 +0,0 @@
-export const objectSchemas = {
-    'employees': Object.assign({}, ...tables.employees.map(field => ({
-        [field.name]: '',
-    })))
-}
